fd8a65004751fcb70c1d2f8044335ddc40eb3f48
Konz <acasadotorres@gmail.com>
2014-04-17 11:08:54 -0500

    First commit

---
+++ LittleDB.Tests/LittleDB.Tests.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Littledb.Tests
+{
+    [TestFixture]
+    public class LittleDbTests
+    {
+        [Test]
+        public void TheDBShouldHaveANameAndCreateADirectory()
+        {
+            new LittleDB("myDatabase");
+            Assert.IsTrue(Directory.Exists(Path.Combine(Directory.GetCurrentDirectory(), "myDatabase")));
+        }
+
+        [Test]
+        public void IShouldBeAbleToInstantiateTwoDatabasesWithSameName()
+        {
+            new LittleDB("myDatabase");
+            new LittleDB("myDatabase");
+        }
+
+        [Test]
+        public void ShouldCreateAfileWithNamedAsTheTypeOfTheObjectSaved()
+        {
+            var littledb = new LittleDB("myDatabase");
+            var me = new Programmer("alberto");
+            littledb.Save(me);
+            Assert.IsTrue(File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "myDatabase", "Programmer")));
+        }
+
+        [Test]
+        public void WhenReadingShouldReturnACollectionOfObjects()
+        {
+            var littledb = new LittleDB("myDatabase");
+            var me = new Programmer("alberto");
+            littledb.Save(me);
+
+            var programmers = littledb.GetAll<Programmer>();
+
+            Assert.That(programmers.Count, Is.EqualTo(1));
+            Assert.That(programmers[0].Name, Is.EqualTo(me.Name));
+        }
+
+        [Test]
+        public void ItShouldReturnAnEmptyListIfNoObjectsSaved()
+        {
+            var littledb = new LittleDB("myDatabase");
+            var programmers = littledb.GetAll<Programmer>();
+
+            Assert.That(programmers.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ICanSaveMultiPleElements()
+        {
+            var littledb = new LittleDB("myDatabase");
+            var me = new Programmer("alberto");
+            var he = new Programmer("marcelo");
+            littledb.Save(me);
+            littledb.Save(he);
+
+            List<Programmer> programmers = littledb.GetAll<Programmer>();
+
+            Assert.That(programmers.Count, Is.EqualTo(2));
+            Assert.That(programmers[0].Name, Is.EqualTo(me.Name));
+            Assert.That(programmers[1].Name, Is.EqualTo(he.Name));
+        }
+
+        [Test]
+        public void CanSaveAList()
+        {
+            var littledb = new LittleDB("myDatabase");
+            var me = new Programmer("alberto");
+            var he = new Programmer("marcelo");
+
+            var programers = new List<Programmer>(){me, he};
+            littledb.SaveList(programers);
+
+            var programmers = littledb.GetAll<Programmer>();
+
+            Assert.That(programmers.Count, Is.EqualTo(2));
+            Assert.That(programmers[0].Name, Is.EqualTo(me.Name));
+            Assert.That(programmers[1].Name, Is.EqualTo(he.Name));
+        }
+
+        [Test]
+        public void ItShouldntCreateNewRegistryWhenSavingObjectsWithSameIdProperty()
+        {
+            var littledb = new LittleDB("myDatabase");
+            var objectWithId = new ObjectWithId(23, "myObject");
+            littledb.Save(objectWithId);
+            littledb.Save(objectWithId);
+
+            littledb.GetAll<ObjectWithId>().Count.ShouldBeEquivalentTo(1);
+        }
+
+        [Test]
+        public void ItShouldGiveIdToObjectsWithThePropertyAndIdIsZero()
+        {
+            var littledb = new LittleDB("myDatabase");
+            var objectWithId = new ObjectWithId(0, "myObject");
+            littledb.Save(objectWithId);
+            littledb.GetAll<ObjectWithId>().Last().Id.ShouldBeEquivalentTo(1);
+
+            var anotherObjectWithId = new ObjectWithId(0, "myObject");
+            littledb.Save(anotherObjectWithId);
+            littledb.GetAll<ObjectWithId>().Last().Id.ShouldBeEquivalentTo(2);
+        }
+
+        [TearDown]
+        public void RemoveDBFolder()
+        {
+            Directory.Delete("myDatabase", true);
+        }
+
+        class Programmer
+        {
+            public string Name { get; set; }
+
+            public Programmer(string name)
+            {
+                Name = name;
+            }
+        }
+
+        class ObjectWithId
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+
+            public ObjectWithId(int id, string name)
+            {
+                Id = id;
+                Name = name;
+            }
+        }
+    }
+}
---
+++ LittleDB/LittleDB.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Littledb
+{
+    public class LittleDB
+    {
+        private readonly string _dbName;
+
+        public LittleDB(string dbName)
+        {
+            _dbName = dbName;
+            Directory.CreateDirectory(_dbName);
+        }
+
+        /// <summary>
+        /// Will save the given element. If has the property Id, will remove any previous record, if the Id is null will give one.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        public virtual void Save<T>(T obj)
+        {
+            RemovePreviousRecordIfId(obj);
+            obj = AddIdIfNeeded(obj);
+            var list = GetAll<T>();
+            list.Add(obj);
+            SaveList(list);
+        }
+
+        /// <summary>
+        /// Will save the entire list, and remove the previous one
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        public void SaveList<T>(List<T> list)
+        {
+            var serializer = new JsonSerializer();
+            using (var sw = new StreamWriter(Path.Combine(_dbName, typeof(T).Name)))
+            using (var writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, list);
+            }
+        }
+
+        /// <summary>
+        /// Returns all the records of the given type
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public virtual List<T> GetAll<T>()
+        {
+            try
+            {
+                var serializer = new JsonSerializer();
+                using (var sw = new StreamReader(Path.Combine(_dbName, typeof(T).Name)))
+                using (var reader = new JsonTextReader(sw))
+                {
+                    return serializer.Deserialize<List<T>>(reader);
+                }
+
+            }
+            catch (FileNotFoundException)
+            {
+                return new List<T>();
+            }
+        }
+
+        protected PropertyInfo GetIdProperty<T>(T obj)
+        {
+            var properties = typeof(T).GetProperties();
+            return properties.SingleOrDefault(p => p.Name.Contains("Id"));
+        }
+
+        protected void RemovePreviousRecordIfId<T>(T obj)
+        {
+            var list = GetAll<T>();
+            var property = GetIdProperty(obj);
+            if (property != null)
+            {
+                var idToRemove = property.GetValue(obj);
+                list.RemoveAll(o => property.GetValue(o).Equals(idToRemove));
+                SaveList(list);
+            }
+        }
+
+        protected T AddIdIfNeeded<T>(T obj)
+        {
+            var idProperty = GetIdProperty(obj);
+            if (idProperty != null && idProperty.GetValue(obj).Equals(0))
+            {
+                var list = GetAll<T>();
+                var currentIds = new List<int>();
+                list.ForEach(o => currentIds.Add((int)idProperty.GetValue(o)));
+
+                var newId = currentIds.Any() ? currentIds.Max() + 1 : 1;
+                idProperty.SetValue(obj, newId);
+            }
+
+            return obj;
+        }
+    }
+}
